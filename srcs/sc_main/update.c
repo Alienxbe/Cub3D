385089886a2d46766eb8b09c539ea6745f2eb94c
Mykman <martin.ykman@gmail.com>
2025-10-24 18:19:05 +0200

    Changed printf to ft_printf

--- srcs/sc_main/update.c
+++ srcs/sc_main/update.c
@@ -6,15 +6,15 @@
 /*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 00:34:25 by marykman          #+#    #+#             */
-/*   Updated: 2025/10/23 15:36:23 by marykman         ###   ########.fr       */
+/*   Updated: 2025/10/24 18:12:55 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include "minimap.h"
 #include "player.h"
 #include "raycast.h"
 #include "sc_main.h"
-#include <stdio.h>
 
 void	main_update(t_sc_main *sc)
 {
@@ -27,6 +27,7 @@ int	sc_main_update(t_sc_main *sc)
 {
 	main_update(sc);
 	main_draw(sc);
-	printf("fps: %d\n", sc->sfe->fps);
+	if (SHOW_FPS)
+		ft_printf("fps: %d\n", sc->sfe->fps);
 	return (sc->running);
 }
