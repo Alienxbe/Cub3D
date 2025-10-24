e7daf6f608346531eb09ac7868f1ff24d201b93c
Mykman <martin.ykman@gmail.com>
2025-10-24 17:48:38 +0200

    Error message for texture files

--- srcs/textures/init.c
+++ srcs/textures/init.c
@@ -6,10 +6,11 @@
 /*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 12:31:44 by marykman          #+#    #+#             */
-/*   Updated: 2025/10/24 15:36:29 by marykman         ###   ########.fr       */
+/*   Updated: 2025/10/24 17:46:11 by marykman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include "sfe_image.h"
 #include "textures.h"
 
@@ -25,7 +26,10 @@ int	textures_init(t_sc_main *sc)
 		text->img = sfe_xpm_file_to_image(sc->sfe->mlx_ptr,
 				sc->game->map.wall_text_path[dir]);
 		if (!text->img.img)
+		{
+			ft_printf("Error on opening textures\n");
 			return (false);
+		}
 		text->ratio = text->img.size.x / CELL_SIZE;
 	}
 	return (true);
